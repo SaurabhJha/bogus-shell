420ed33a0dd6fa9e3125e51c30ba1d44146bcbc5
Saurabh Jha <saurabh.jhaa@gmail.com>
2016-01-12 11:03:36 +0530

    Set basic command line discipline.

--- shell.c
+++ shell.c
@@ -1,12 +1,22 @@
+#include <stdio.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <signal.h>
 #include "shell.h"
 
-bool shell_is_interactive;
 int shell_terminal;
+bool shell_is_interactive;
+pid_t shell_pid;
 
-int main (int argc, char* argv[])
+int main (int argc, char *argv[])
 {
   shell_terminal = STDIN_FILENO;
   shell_is_interactive = isatty(shell_terminal);
+
+  if (shell_is_interactive) {
+    while (tcgetpgrp(shell_terminal) != (shell_pid = getpgrp()))
+      kill(-shell_pid, SIGTTIN);
+    tcsetpgrp(shell_terminal, shell_pid);
+  }
 }
--- shell.h
+++ shell.h
@@ -1,4 +1,8 @@
 #include <stdbool.h>
+#include <termios.h>
+#include <sys/types.h>
 
-extern bool shell_is_interactive;
 extern int shell_terminal;
+extern bool shell_is_interactive;
+extern pid_t shell_pid;
+extern struct termios shell_tmodes;
